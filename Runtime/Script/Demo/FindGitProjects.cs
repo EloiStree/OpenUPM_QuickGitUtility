e49a772da3c3e01c9690d269d46ed36428547d4a
streeeloi <streeeloi@gmail.com>
2019-07-25 05:18:20 +0200

    Save: 2019-18-25 -  05:18

---
+++ Runtime/Script/Demo/FindGitProjects.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FindGitProjects : MonoBehaviour
+{
+    public string m_directory;
+    public string[] m_gitLinksPath;
+    public List<GitLinkOnDisk > m_gitLinks;
+
+    private void Start()
+    {
+        if (string.IsNullOrEmpty(m_directory))
+            return;
+        m_gitLinksPath = QuickGit.GetAllFolders(m_directory,true);
+        m_gitLinks = QuickGit.GetGitProjectsInDirectory(m_directory);
+    }
+}
--- Runtime/Script/Shared/Gist/QuickGit.cs
+++ Runtime/Script/Shared/Gist/QuickGit.cs
@@ -18,20 +18,42 @@ public static class QuickGit
         for (int i = 0; i < folders.Count; i++)
         {
             string path = folders[i];
-            UnityEngine.Debug.Log("d " + path);
-            bool isEmpty = Directory.GetFiles(path).Length <= 0;
-            string emptyPath = path + "/empty.txt";
+            string[] files= Directory.GetFiles(path);
+            string emptyPath = path + "\\empty.txt";
+            string emptyPathMeta = path + "\\empty.txt.meta";
+            files=Remove( files, emptyPathMeta, emptyPath);
+//            UnityEngine.Debug.Log("<> " + path + " : " + files.Length);
+            
+            bool isEmpty = files.Length <= 0;
+
             if (isEmpty)
             {
                 File.WriteAllText(emptyPath, "Avoid empty folder");
             }
-            else {
-                if(File.Exists(emptyPath))
+            else
+            {
+                if (File.Exists(emptyPath))
                     File.Delete(emptyPath);
+                if (File.Exists(emptyPathMeta))
+                    File.Delete(emptyPathMeta);
             }
         }
     }
 
+    private static string[] Remove( string[] files, string emptyPathMeta, string emptyPath)
+    {
+        List<string> f = new List<string>();
+        for (int i = 0; i < files.Length; i++)
+        {
+            if (files[i] == emptyPath || files[i] == emptyPathMeta)
+            { }
+            else {
+                f.Add(files[i]);
+            }
+
+        }
+        return f.ToArray();
+    }
 
     public static List<GitLinkOnDisk> GetGitProjectsInDirectory(string directoryPath)
     {
@@ -49,25 +71,7 @@ public static class QuickGit
         }
         return packages;
     }
-    public static List<GitUnityPackageLinkOnDisk> GetGitUnityPackageInDirectory(string directoryPath)
-    {
-        return GetGitUnityPackageInDirectory(GetGitProjectsInDirectory(directoryPath));
-    }
-    public static List<GitUnityPackageLinkOnDisk> GetGitUnityPackageInDirectory(string[] directoriesPath)
-    {
-        return GetGitUnityPackageInDirectory(GetGitProjectsInDirectory(directoriesPath));
-    }
-    public static List<GitUnityPackageLinkOnDisk> GetGitUnityPackageInDirectory(List<GitLinkOnDisk> packages)
-    {
-        List<GitUnityPackageLinkOnDisk> p = new List<GitUnityPackageLinkOnDisk>();
-        for (int i = 0; i < packages.Count; i++)
-        {
-            p.Add(new GitUnityPackageLinkOnDisk(packages[i].m_projectDirectoryPath));
-
-        }
-        return p;
-    }
-
+   
     private static bool IsFolderContainGitProject(string p)
     {
         string[] directories = Directory.GetDirectories(p);
@@ -302,14 +306,7 @@ public static class QuickGit
         files.AddRange(pathfilesOwn);
         RunCommands(files.ToArray(), directoryPath);
     }
-    public static bool GetUnityPackage(string directoryPath, out GitUnityPackageJson package)
-    {
-        package = null;
-        string filePath = directoryPath + "/package.json";
-        if (File.Exists(filePath))
-            package = GitUnityPackageJson.CreateFromFile(filePath);
-        return package != null;
-    }
+   
     public static bool GetGitUrl(string directoryPath, out string url)
     {
         url = "";
@@ -355,115 +352,5 @@ public class GitLinkOnDisk : GitLink
     public bool IsPathDefined() { return !string.IsNullOrWhiteSpace(m_projectDirectoryPath); }
 
 }
-[System.Serializable]
-public class GitUnityPackageLink : GitLink
-{
-    public GitUnityPackageJson m_packageInfo;
-    public bool IsPackageDefined() { return m_packageInfo != null && !string.IsNullOrWhiteSpace(m_packageInfo.GetNamespaceID()); }
-}
-[System.Serializable]
-public class GitUnityPackageLinkOnDisk : GitUnityPackageLink
-{
 
-    public string m_projectDirectoryPath;
 
-    public GitUnityPackageLinkOnDisk(string directoryPath)
-    {
-
-        QuickGit.GetUnityPackage(directoryPath, out m_packageInfo);
-        QuickGit.GetGitUrl(directoryPath, out m_gitLink);
-        this.m_projectDirectoryPath = directoryPath;
-        
-
-    }
-    public bool IsPathDefined() { return !string.IsNullOrWhiteSpace(m_projectDirectoryPath); }
-
-}
-
-[System.Serializable]
-public class GitUnityPackageJson
-{
-    //"name": "be.eloiexperiments.unitypackagefacilitator",                              
-    public string name;
-    public string GetNamespaceID() { return name; }
-    //"displayName": "Unity Package Creator",                        
-    public string displayName;
-    public string GetDisplayName() { return displayName; }
-    //"version": "0.0.1",                         
-    public string version;
-    public string GetPackageVersion() { return version; }
-    //"unity": "2018.1",                             
-    public string unity;
-    public string GetUnityVersion() { return unity; }
-    //"description": "Tools to create the structure of new unity package under 2 minutes.",                         
-    public string description;
-    public string GetDescriptionVersion() { return description; }
-    //"keywords": ["Script","Tool","Productivity","Git","Unity Package"],                       
-    public string [] keywords;
-    public string [] GetKeywords() { return keywords; }
-    //"category": "Script",                   
-    public string category;
-    public string GetCategory() {
-        return category;
-    }
-    //"dependencies":{}   
-//    public List<DependenciyJson > dependencies;
-
-    public string ToJson() { return JsonUtility.ToJson(this); }
-    public static GitUnityPackageJson CreateFromFile(string filePath) {
-        string file = File.ReadAllText(filePath);
-        return CreateFromJson(file);
-    }
-    
-        public static GitUnityPackageJson CreateFromJson(string json) {
-        try
-        {
-            UnityEngine.Debug.Log("json: " + json);
-            return JsonUtility.FromJson<GitUnityPackageJson>(json);
-        }
-        catch (Exception e) {
-            UnityEngine.Debug.LogWarning(e);
-        }
-        return null;
-    }
-}
-[System.Serializable]
-public class DependenciyJson
-{
-    public string nameId;
-    public string value;
-
-    public string GetNamespaceId() { return nameId; }
-    public bool GetLink(out string url) {
-        bool isVersion = IsVersionSet();
-        url = "";
-        if (!isVersion) {
-            url = value;
-        }
-        return !isVersion;
-    }
-
-    public bool IsVersionSet()
-    {
-        int v1,v2,v3;
-        return GetVersion(out v1, out v2, out v3);
-    }
-    public bool GetVersion(out int v1, out int v2, out int v3)
-    {
-        v1 = v2 = v3 = 0;
-        string[] tokens = value.Split('.');
-        if (tokens.Length == 3) {
-
-            try
-            {
-                v1 = int.Parse(tokens[0]);
-                v2 = int.Parse(tokens[1]);
-                v3 = int.Parse(tokens[2]);
-
-            } catch (Exception) {
-                return false;
-            }
-        }
-        return true;
-    }
-}
