28ec2916cdcf7d65fd9d66776597cadb24ffefe2
eloi <eloistree@gmail.com>
2020-05-29 03:16:25 +0200

    Save: 05/29/2020 3:16 AM

--- Runtime/Core/QuickGit.cs
+++ Runtime/Core/QuickGit.cs
@@ -1,11 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.IO;
 using System.Linq;
-using System.Security.Policy;
 using UnityEngine;
-using UnityEngine.EventSystems;
 
 public static class QuickGit
 {
