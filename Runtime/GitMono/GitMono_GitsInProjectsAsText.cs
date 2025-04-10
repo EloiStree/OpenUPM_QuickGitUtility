34d6327cfd264e2a5bc9e3cf902dead63f11a103
Eloi Teaching <eloiteaching@gmail.com>
2025-04-10 23:03:56 +0200

    Save: 2025_04_10_23_03

--- Runtime/GitMono/GitMono_GitsInProjectsAsText.cs
+++ Runtime/GitMono/GitMono_GitsInProjectsAsText.cs
@@ -5,9 +5,15 @@ namespace Eloi.Git
 {
     public class GitMono_GitsInProjectsAsText : MonoBehaviour {
 
-    public string m_unityAssetPath;
+        public bool m_useGitCloneInFrontOf = true;
+        private void Reset()
+        {
+            Refresh();
+        }
+        public string m_unityAssetPath;
     public List<GitLinkOnDisk> m_inParentsPath;
     public List<GitLinkOnDisk> m_inTheProject;
+    public string m_gitAsTextList;
 
     [ContextMenu("Refresh")]
     public void Refresh()
@@ -15,9 +21,19 @@ namespace Eloi.Git
         string assetPath = UnityPaths.GetUnityAssetsPath();
         QuickGit.GetGitsInParents(assetPath, out m_inParentsPath);
         QuickGit.GetGitsInDirectory(assetPath, out m_inTheProject, true);
-    }
+            m_gitAsTextList = "```\n";
+            foreach (var item in m_inTheProject)
+                if (m_useGitCloneInFrontOf)
+                    m_gitAsTextList +="git clone "+ item.m_gitLink + "\n";
+                else 
+                    m_gitAsTextList += item.m_gitLink + "\n";
 
-}
+            m_gitAsTextList += "```\n";
+
+
+        }
+
+    }
 
 
 
