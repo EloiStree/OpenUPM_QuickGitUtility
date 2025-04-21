341767d0028e23f667b7ae5c8dcc642e4378b98d
Éloi Strée <eloistree@gmail.com>
2025-04-21 14:49:35 +0200

    Save: 2025_04_21_14_49

---
+++ Runtime/GitMono/GitMono_MakeGitLinksClonable.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace Eloi.Git
+{
+    public class GitMono_MakeGitLinksClonable : MonoBehaviour 
+    {
+        [TextArea(0,10)]
+        public string m_gitLinks;
+        public List<string> m_gitLinksList;
+        [TextArea(0, 10)]
+        public string m_gitLinksClonable;
+
+        [ContextMenu("Do The Thing")]
+        public void DoTheThing() {
+
+
+            m_gitLinksList = m_gitLinks.Split(new[] { "\n", "\r\n" }, System.StringSplitOptions.RemoveEmptyEntries).ToList();
+            m_gitLinksClonable = "";
+            foreach (string link in m_gitLinksList)
+            {
+                if (string.IsNullOrEmpty(link))
+                    continue;
+                if (!link.Contains(".git"))
+                    continue;
+                string [] splitBySlash = link.Split('/');
+                if (splitBySlash.Length < 2)
+                    continue;
+                string gitDirectory = string.Join("/", splitBySlash[splitBySlash.Length - 2], splitBySlash[splitBySlash.Length - 1].Split("#").First().Replace(".git",""));
+                string clone = "git clone " + link.Trim() + " " + gitDirectory.Trim() + "\n\r";
+                m_gitLinksClonable += clone;
+
+            }
+        }
+
+
+
+    }
+
+}
---
+++ Runtime/GitMono/GitMono_RemoveAllDotGitInFolder.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using UnityEngine;
+
+namespace Eloi.Git
+{
+    public class GitMono_RemoveAllDotGitInFolder: MonoBehaviour
+    {
+        public string m_folderPath;
+        public RemoveType m_removeType = RemoveType.Delete;
+        public enum RemoveType { Delete, RenameAsGitI}
+
+
+        public string[] directories;
+        public List<string> folderToRemove;
+        [ContextMenu("DoTheThing")]
+        public void DoTheThing()
+        {
+            directories= System.IO.Directory.GetDirectories(m_folderPath, "*", System.IO.SearchOption.AllDirectories);
+            folderToRemove= new List<string>();
+            foreach (string directory in directories)
+            {
+                // if the folder parent is /.git 
+                if (directory.EndsWith(".git"))
+                {
+                    folderToRemove.Add(directory);
+                }
+             
+            }
+            folderToRemove = folderToRemove.Distinct().ToList();
+
+            foreach (string folder in folderToRemove)
+            {
+                try
+                {
+                    if (m_removeType == RemoveType.Delete)
+                    {
+                        System.IO.Directory.Delete(folder, true);
+                    }
+                    else
+                    {
+                        string newName = folder.Replace(".git", ".giti");
+                        System.IO.Directory.Move(folder, newName);
+                        System.IO.Directory.Delete(folder, true);
+
+                    }
+                }
+                catch (System.Exception e)
+                {
+                    Debug.LogError("Error removing folder: " + folder + "\n" + e.Message);
+                }
+            }
+        }
+    }
+
+}
